571277d6f48a02f7fdf84acb6dd05dfd36a19a7b
Sandra Sanders <sandy-sanders@msn.com>
2024-08-12 21:50:12 -0700

    set up background

--- coffee-store/tailwind.config.ts
+++ coffee-store/tailwind.config.ts
@@ -9,12 +9,24 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+        gradient: 'url("/static/background.png")',
     },
+      colors: {
+        purple: {
+        "951": "#5755eb",
+      },
+      gray: {
+        951: 'hsla(0, 0%, 100%, 0.04)',
+        952: '#373b64',
+      },
+      
+    }
+
+
+  }
+    
   },
   plugins: [],
 };
+
 export default config;
