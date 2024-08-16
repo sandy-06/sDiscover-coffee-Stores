30019a2135e1f5bbed5d876e76db4d627896f993
Sandra Sanders <sandy-sanders@msn.com>
2024-08-15 19:08:03 -0700

    routes started

--- coffee-store/app/page.tsx
+++ coffee-store/app/page.tsx
@@ -1,11 +1,16 @@
-'use client';  
+ 'use client'
 import Banner from "@/components/banner.client";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
+  const coffeeStoreId =   'dark-horse-coffee';
   return (
+  <div className='mb-56'>
     <main className="flex min-h-screen flex-col items-center justify-between p-14" >
       <Banner />
+      <Link href={`/coffee-store/${coffeeStoreId}`}>Dark Horse coffee</Link>
     </main>
-  )
+  </div>
+  );
 }
